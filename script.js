196ad8d92ee18952ba83734d410f845b4361c3c4
pritvorjared <pritvorjared@gmail.com>
2022-08-30 22:46:06 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// const hamburger = 5;
+// const fries = 0;
+
+// if (hamburger && fries) {
+//   console.log("Ya Sut");
+// }
+
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 1;
+
+// console.log(hamburger === 3 && cola && fries);
+
+// console.log(1 && 0);
+// console.log(1 && 5);
+// console.log(null && 5);
+// console.log(5 && "sfsdffds");
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//   console.log("Все сыты");
+// } else {
+//   console.log("Мы уходим");
+// }
+
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if ((hamburger === 3 && cola === 2) || (fries === 3 && nuggets)) {
+  console.log("Все сыты");
+} else {
+  console.log("Мы уходим");
+}
+
+console.log((hamburger === 3 && cola === 2) || (fries === 3 && nuggets));
+
+// let johnReport,
+//   alexReport,
+//   samReport,
+//   mariaReport = "done";
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
